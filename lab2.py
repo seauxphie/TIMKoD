fa3dc4da6058b9589e03b12278838378ea8ed38b
Zofia Tarant <zosiarrf@gmail.com>
2019-05-15 14:04:24 +0200

    lab2 complete

--- lab2.py
+++ lab2.py
@@ -1,6 +1,6 @@
 import random
 
-def count_subarray(subarr, arr): #znajduje ile razy subarr znajduje się w arr (zachowana kolejność wyrazow)
+def count_subarray(subarr, arr): 
     count=0
     j=0
     for i in range(len(arr)):
@@ -16,8 +16,9 @@ def count_subarray(subarr, arr): #znajduje ile razy subarr znajduje się w arr (
     return count
 
 
-def get_ngram_counts(text, n): #znajduje wszystkie ngramy i zapisuje ile ich jest w tekście
+def get_ngram_counts(text, n): 
     words=text.split(' ')
+    
     dic={}
     counts=[]
     ngrams=[]
@@ -33,20 +34,33 @@ def get_ngram_counts(text, n): #znajduje wszystkie ngramy i zapisuje ile ich jes
         ngrams.append(key)
     return dic
 
-def get_probabilities(ngrams, begins_with): #zllicza ngramy zaczynające się na chars_before 
-    words=[]                              #i liiczy prawdopodobieństwo wystąpienia kolejnej litery po tyvh znakach
+def get_ngram_counts_with_first(text, n, first_word): 
+    words=text.split(' ')
+    dic={}
+    counts=[]
+    ngrams=[]
+    for i in range(0, len(words)-n+1):
+        ngram_arr=words[i:i+n]
+        ngram=tuple(ngram_arr)
+        if dic.get(ngram) == None and ngram_arr[0]==first_word:
+            dic[ngram]=count_subarray(ngram_arr, words)
+    for key, value in dic.items():
+        counts.append(value)
+        ngrams.append(key)
+    return dic
+
+def get_probabilities(ngrams, begins_with):  
+    words=[]                              
     probabilities=[]
     count_all=0
     begins_with=tuple(begins_with)
     for key, value in ngrams.items():
-
-        if key[:-1] == begins_with:
+        if key[:-1] == begins_with and value > 0:
             words.append(key[-1])
             probabilities.append(value)
             count_all+=value
     for i in range(len(probabilities)):
         probabilities[i]=probabilities[i]/count_all
-    #print(sum(probabilities))
     return words, probabilities
 
 def generate_using_ngrams(basic_text, n, length):
@@ -55,21 +69,38 @@ def generate_using_ngrams(basic_text, n, length):
     for i in range(n-1, length):   
 
         words, probs = get_probabilities(ngrams, text[i-n+1:i])
+        if len(words) == 0:
+            break
         word = random.choices(words, probs)[0]
-
         text.append(word)
 
-        """if len(letters) == 0:
-            text+=generate_using_bigrams(basic_text,2,text[i-1])[1]
-        else:
-            text+=random.choices(letters, probs)[0]
-        """
     result=(' ').join(text)
     return result
-file=open("norm_romeo_and_juliet.txt",'r')
-text=file.read()[:6000]
+
+
+def generate_with_1st_word(basic_text, length, begins_with):
+    text=[begins_with]
+    ngrams = get_ngram_counts(basic_text, 2)
+    words, probs = get_probabilities(ngrams, [begins_with])
+    text.append(random.choices(words, probs)[0])
+    ngrams = get_ngram_counts(basic_text, 3)
+    print(text)
+    for i in range(2, length): 
+        words, probs = get_probabilities(ngrams, text[i-2:i])
+        if len(words) == 0:
+            break
+        word = random.choices(words, probs)[0]
+        text.append(word)
+
+    result=(' ').join(text)
+    return result
+
+file=open("wiki.txt",'r')
+text=file.read()
 
 print("Tekst wygenerowany na podstawie 1 poprzedniego wyrazu:")
 print(generate_using_ngrams(text, 2, 100))
 print("\nTekst wygenerowany na podstawie 2 poprzednich wyrazów:")
 print(generate_using_ngrams(text, 3, 100))
+print("\nTekst wygenerowany na podstawie 2 poprzednich wyrazów z pierwszym 'probability':")
+print(generate_with_1st_word(text, 100, 'probability'))
