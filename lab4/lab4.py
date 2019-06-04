1f0a3531fe6d1c6602c0e0ed0d58007c3ff436c1
Zofia Tarant <zosiarrf@gmail.com>
2019-06-04 18:41:43 +0200

    complete lab4

---
+++ lab4/lab4.py
@@ -0,0 +1,60 @@
+import string
+from bitarray import bitarray
+#from bitstring import BitArray
+
+#letters_to_bits={}
+#bits_to_letters={}
+
+
+#create code
+def create(frequency):
+    i=0
+    letters_to_bits={}
+    bits_to_letters={}
+    for _, c in frequency:
+        s=bin(i)[2:]
+        while len(s)<6:
+            s='0'+s
+        letters_to_bits[c]=bitarray(s)
+        temp = bitarray(s).tobytes()
+        bits_to_letters[temp]=c
+        i+=1
+    return letters_to_bits, bits_to_letters
+
+def encode(text, code):
+    encode_text=bitarray()
+    for c in text:
+        encode_text+=code[c]
+    return encode_text
+
+def decode(text, code):
+    decoded_text=""
+    for i in range(0,len(text),6):
+        temp = text[i:i+6].tobytes()
+        decoded_text += code[temp]
+    return decoded_text
+
+def save(text, code):
+    compressed_file = open("encoded", 'wb')
+    encoded_text=encode(text, code)
+    encoded_text.tofile(compressed_file)
+    return encoded_text
+
+def load(text, code):
+    decoded_text = decode(text, code)
+    return decoded_text
+
+def frequency(text):
+    chars='0123456789 '+string.ascii_lowercase
+    length=len(text)
+    result = [(text.count(c)/length, c) for c in chars]
+    result.sort(reverse=True)
+    return result
+
+
+file=open("norm_wiki_sample.txt",'r')
+text=file.read()
+letters_to_bits, bits_to_letters = create(frequency(text))
+encoded = save(text, letters_to_bits)
+decoded = load(encoded, bits_to_letters)
+print(decoded)
